66f18a4242f81c3539e769e9138f7561bd95815f
MauriDev <echanizmauricio@gmail.com>
2023-05-24 16:01:06 -0300

    borrado comentarios toys.js

--- assets/JsPages/toys.js
+++ assets/JsPages/toys.js
@@ -1,37 +1,3 @@
-// const {createApp} = Vue;
-
-// const app = createApp({
-
-//   data(){
-//     return{
-//       arrayEvents:[],
-//       id:null,
-//       event:{}
-//     }
-//   },
-
-//   created(){
-
-//     fetch(`https://mindhub-xj03.onrender.com/api/amazing`)
-//     .then(res => res.json())
-//     .then(data =>{
-//       this.arrayEvents = data.events
-
-//       const params = new URLSearchParams(location.search)
-
-//       this.id = params.get(`id`)
-
-//       this.event = this.arrayEvents.find(event=>event._id == this.id)
-
-//       console.log(this.event)
-
-
-//     }).catch(error => console.log(error))
-
-//   }
-
-// }).mount(`#app`)
-
 const {createApp} = Vue;
 
 const app = createApp ({
